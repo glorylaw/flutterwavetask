010fdec4042e869216e33b1b99c4469ceaecba08
Glory Lawrence <100371169+glorylaw@users.noreply.github.com>
2023-09-01 14:23:08 +0100

    app running

--- src/route/accountRoute.ts
+++ src/route/accountRoute.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import AccountController from '../controller/accountController';
 
+import { Request, Response } from 'express';
+
 const router: Router = Router();
 const accountController = new AccountController();
 
+router.get("/", (req:Request,res:Response)=>{
+    res.status(200).send("App is currectly running")
+
+})
 router.post('/create', accountController.createAccount);
 
 router.get('/resolve/:accountNumber', accountController.getSingleAccount);
